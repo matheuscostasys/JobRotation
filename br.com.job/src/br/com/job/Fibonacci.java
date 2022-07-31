a0c3ae65491002f330a5ecf59aecb35e042d3589
Matheus Costa <52360057+matheuscostasys@users.noreply.github.com>
2022-07-31 15:52:04 -0300

    ADD
    adicionando  o codigo do fibonacci

---
+++ br.com.job/src/br/com/job/Fibonacci.java
@@ -0,0 +1,23 @@
+package br.com.job;
+
+public class Fibonacci {
+
+	 static long fibo(int n) {
+	        if (n < 2) {
+	            return n;
+	        } else {
+	            return fibo(n - 1) + fibo(n - 2);
+	        }
+	    }
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 30; i++) {
+            System.out.print("(" + i + "):" + Fibonacci.fibo(i) + "\t");
+        }
+
+    }
+
+
+	
+
+}
